336cd51d470be35b95425e027297d8617f1a5d54
Kush2205 <kr.kushagra2205@gmail.com>
2024-08-19 22:38:29 +0530

    first commit

---
+++ Urjotsav/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    
+
+    return (
+        <>
+            <h1 className='text-center text-3xl'>Header</h1>
+        </>
+    )
+}
