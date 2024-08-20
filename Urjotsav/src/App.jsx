d59af4f7d9a80bde14a7b04e1aa111298e70fd56
Mayank Kumar Maurya <161640382+Mayank-0518@users.noreply.github.com>
2024-08-20 07:09:47 +0000

    ............

--- Urjotsav/src/App.jsx
+++ Urjotsav/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import LandingPage from "./images/Landing Page.png"
-
+import Designer from "./images/Designer.png"
+import '../src/App.css'
+import Cursor from '../src/components/Cursor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    
-    <div className='w-full h-screen bg-no-repeat bg-cover' style={{ backgroundImage:`url(${LandingPage})` }}>
+    <div id='bgimg' className=' w-full h-screen bg-no-repeat bg-cover bg-center overflow-hidden' style={{ backgroundImage:`url(${LandingPage})`}}>
    <Header/>
-    
+   
+   <div className='h-[100%] flex justify-center items-center '>
+      <Cursor/>
+   </div>
     </div>
     
    
