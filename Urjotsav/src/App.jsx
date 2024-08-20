2baf65fbcab054da0cfb610839585b4edb60bd68
Kush2205 <169088503+Kush2205@users.noreply.github.com>
2024-08-20 10:59:09 +0000

    second

--- Urjotsav/src/App.jsx
+++ Urjotsav/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import Header from './components/Header'
 import LandingPage from "./images/Landing Page.png"
 import Designer from "./images/Designer.png"
@@ -6,14 +6,14 @@ import '../src/App.css'
 import Cursor from '../src/components/Cursor'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
    
     <div id='bgimg' className=' w-full h-screen bg-no-repeat bg-cover bg-center overflow-hidden' style={{ backgroundImage:`url(${LandingPage})`}}>
    <Header/>
    
-   <div className='h-[100%] flex justify-center items-center '>
+   <div className='h-[100%] flex justify-center items-center  '>
       <Cursor/>
    </div>
     </div>
