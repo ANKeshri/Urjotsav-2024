e199b0d8e0ce11a980c79d92a66818d1f2ab3fda
Kush2205 <169088503+Kush2205@users.noreply.github.com>
2024-08-24 12:31:43 +0000

    fixed background

--- Urjotsav/src/App.jsx
+++ Urjotsav/src/App.jsx
@@ -1,36 +1,51 @@
-
+import { useState } from 'react'
 import Header from './components/Header'
 import Layer1 from "./images/Layer1.jpg"
 import '../src/App.css'
 import Cursor from '../src/components/Cursor'
 import planet from "../src/images/planet.png"
 import { Parallax, ParallaxLayer } from "@react-spring/parallax"
-
+import { gsap } from 'gsap'
+import {useGSAP} from "@gsap/react"
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 function App() {
 
- 
+  gsap.registerPlugin(useGSAP)
+  gsap.registerPlugin(ScrollTrigger)
 
+  useGSAP(() => {
+       gsap.to("#image",{
+         
+          scale:3,
+          y:100,
+          scrollTrigger:{
+            trigger:"#image",
+            start:"top 15%",
+            end:"bottom",
+            scrub:true,
+            fastScrollEnd:true,
+           
+            
+          }
+       })
+  })
+   
   return (
 
     <>
-    <div>
-      <div className='w-full h-screen' style={{backgroundImage:`url(${Layer1})`,backgrounPosition:"center",backgroundSize:"cover"}}>
-     
-        <Parallax pages={2}>
-          <ParallaxLayer style={{zIndex:1}}>
-            <Header/>
-          </ParallaxLayer>
-          
-          <ParallaxLayer offset={0} >
-             <div className='flex h-screen items-end justify-center'>
-              <img id='image' className='w-[75%]' src="../src/images/planet.png" alt="Logo" />
-             </div>
-          </ParallaxLayer>
-           
-        </Parallax>
+     <div className='w-full h-[200vh]' >
+      <div style={{backgroundImage:`url(${Layer1})`,backgroundPosition:"center",backgroundSize:"cover"}} className='w-full h-screen'>
+       <Header/>
+       <div className='h-screen flex p-14  justify-center'>
+        <img id='image' src='../src/images/planet.png'></img>
+       </div>
       </div>
-    </div>
+
+      <div style={{backgroundImage:`url(${Layer1})`,backgroundPosition:"center",backgroundSize:"cover"}} className='w-full h-screen'>
+
+      </div>
+     </div>
     </>
 
 
