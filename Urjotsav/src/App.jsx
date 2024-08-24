586e25eb00b6e515e746945124ca8c1c0e38142d
Kush2205 <169088503+Kush2205@users.noreply.github.com>
2024-08-24 16:03:17 +0000

    added animation to explore more button

--- Urjotsav/src/App.jsx
+++ Urjotsav/src/App.jsx
@@ -59,7 +59,7 @@ function App() {
           <div id='Urjotsav_title' className='h-auto w-[75vw] ' style={{ textAlign: 'center', color: 'white', fontSize: 150, fontFamily: 'Megrim', fontWeight: '500', letterSpacing: 6, wordWrap: 'break-word' }}>ABOUT US</div>
           <div className='h-auto w-[79vw] z-20 relative' style={{ textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Jost', fontWeight: '300', letterSpacing: 1.20, wordWrap: 'break-word' }}>Urjotsav, a prestigious national-level inter-collegiate annual technical and entrepreneurial festival hosted by the Rajiv Gandhi Institute of Petroleum Technology, is orchestrated with meticulous care by the Science and Technical Council on an annual basis. This extraordinary endeavor stands as a testament to the proactive dedication of our students, who strive ceaselessly to craft an event that not only exudes grandeur but also sparks profound intellectual curiosity within the minds of the intellectually inclined.<br />The festival is deeply committed to furnishing a robust platform wherein the potential industry leaders of tomorrow can unveil their prodigious talents, and where the young minds of today can engage in collaboration, strategic planning, and execute a spectacular celebration, thus instilling in them invaluable leadership skills and fostering the spirit of teamwork among its participants.</div>
 
-          <button className='border-gray-50  rounded-full  text-white text-2xl z-30 mt-7 p-3 mb-5 ' style={{ borderWidth: `2px` }}>Explore more</button>
+          <button className='border-gray-50  rounded-full  text-white text-2xl z-30 mt-7 p-3 mb-5 transition-all hover:bg-white hover:text-black' style={{ borderWidth: `2px` }}>Explore more</button>
         </div>
 
       </div>
