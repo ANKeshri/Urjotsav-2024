c53ad310d1fcbbf91757551f6bb5aeb3e0daeffa
Kush2205 <169088503+Kush2205@users.noreply.github.com>
2024-08-25 13:04:48 +0000

    Changed file structure

--- Urjotsav/src/App.jsx
+++ Urjotsav/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react';
 import Header from './components/Header';
 import Layer1 from "./images/Layer1.jpg";
 import '../src/App.css';
 import About from '../src/components/About';
 import planet from "../src/images/planet.png";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import { gsap } from 'gsap';
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import Contact from './components/Contact';
 
 function App() {
 
@@ -16,7 +15,7 @@ function App() {
 
   useGSAP(() => {
     gsap.to("#image", {
-      y: 200,
+     y:50,
       scale: 3.2,
       scrollTrigger: {
         trigger: "#image",
@@ -26,6 +25,7 @@ function App() {
         fastScrollEnd: true,
       }
     });
+ 
     gsap.to("#name", {
       y: -200,
       scrollTrigger: {
@@ -37,6 +37,7 @@ function App() {
       }
     });
 
+   
   });
 
   return (
@@ -47,20 +48,15 @@ function App() {
           <Header />
 
           <div id='name' className='flex items-center justify-center h-screen w-full absolute'>
-            <h1 className='text-9xl text-white font-Megrim'>URJOTSAV</h1>
+            <h1 id='title' className='text-9xl text-white font-Megrim'>URJOTSAV</h1>
           </div>
 
           <div className='h-screen flex p-16 justify-center mt-4'>
             <img id='image' src={planet} className='relative z-10' alt="Planet" />
           </div>
         </div>
-
-        <div className=' flex items-center justify-center flex-col mt-10 '>
-          <div id='Urjotsav_title' className='h-auto w-[75vw] ' style={{ textAlign: 'center', color: 'white', fontSize: 150, fontFamily: 'Megrim', fontWeight: '500', letterSpacing: 6, wordWrap: 'break-word' }}>ABOUT US</div>
-          <div className='h-auto w-[79vw] z-20 relative' style={{ textAlign: 'center', color: 'white', fontSize: 30, fontFamily: 'Jost', fontWeight: '300', letterSpacing: 1.20, wordWrap: 'break-word' }}>Urjotsav, a prestigious national-level inter-collegiate annual technical and entrepreneurial festival hosted by the Rajiv Gandhi Institute of Petroleum Technology, is orchestrated with meticulous care by the Science and Technical Council on an annual basis. This extraordinary endeavor stands as a testament to the proactive dedication of our students, who strive ceaselessly to craft an event that not only exudes grandeur but also sparks profound intellectual curiosity within the minds of the intellectually inclined.<br />The festival is deeply committed to furnishing a robust platform wherein the potential industry leaders of tomorrow can unveil their prodigious talents, and where the young minds of today can engage in collaboration, strategic planning, and execute a spectacular celebration, thus instilling in them invaluable leadership skills and fostering the spirit of teamwork among its participants.</div>
-
-          <button className='border-gray-50  rounded-full  text-white text-2xl z-30 mt-7 p-3 mb-5 transition-all hover:bg-white hover:text-black' style={{ borderWidth: `2px` }}>Explore more</button>
-        </div>
+        <About/>
+        <Contact/>
 
       </div>
     </>
--- Urjotsav/src/components/Header.jsx
+++ Urjotsav/src/components/Header.jsx
@@ -6,10 +6,11 @@ export default function Header() {
     return (
        
 
-           <div className='flex space-x-20 text-white justify-end  mr-10 py-4 '>
+           <div className='flex space-x-20 text-white justify-center  mr-10 py-4 '>
         
             <div className='flex transition-all hover:cursor-pointer hover:scale-125 font-Jost '>HOME</div>
            <div className='flex transition-all hover:cursor-pointer hover:scale-125 font-Jost '>ABOUT US</div>
+           <div className='flex transition-all hover:cursor-pointer hover:scale-125 font-Jost '>EVENTS</div>
            <div className='flex transition-all hover:cursor-pointer hover:scale-125 font-Jost '>CONTACT</div>
            <div className='flex transition-all hover:cursor-pointer hover:scale-125 font-Jost ' >BROCHURE</div>
            
