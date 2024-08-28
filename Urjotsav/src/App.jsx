e508fc90f25693358c43ff907c77d02d8c1ae9d9
Kush2205 <169088503+Kush2205@users.noreply.github.com>
2024-08-28 16:43:51 +0000

    minor changes

--- Urjotsav/src/App.jsx
+++ Urjotsav/src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { gsap } from 'gsap';
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Layout from './Layout';
 import About from './components/About';
 import Contact from './components/Contact';
-import planet from "../src/images/planet.png"
 import Home from './components/Home';
 
 
@@ -19,8 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home/>}/>
-        
-          <Route path="contact" element={<Contact />} />
+            <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </Router>
